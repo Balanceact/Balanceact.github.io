e72226823ab2dc0e9b08817aa1b07a3180d7a0b9
Balanceact <makildur@hotmail.com>
2024-07-31 14:57:23 +0200

    Refactored code to v0.2

--- index.js
+++ index.js
@@ -3,27 +3,36 @@ const result = document.querySelector(".result");
 const icons =  document.querySelector(".icons");
 const willpowerReRoll = document.querySelector(".btn-WPRR");
 
-formRoller.addEventListener("submit", (event) => {
-    event.preventDefault();
+let black = [];
+let red = [];
+let successes = 0;
+let crits = 0;
+let rerollable = 0;
 
-    const hunger = formRoller.querySelector("#hunger").value;
-    const pool = formRoller.querySelector("#pool").value - hunger;
+function vampireDice() {
+    return Math.floor(Math.random() * 10) + 1;
+}
 
-    let black = [pool];
-    let red = [hunger];
-    let successes = 0;
-    let crits = 0;
-
+function fillRed(hunger) {
     for (let index = 0; index < hunger; index++) {
-        red[index] = Math.floor(Math.random() * 10) + 1;
+        red[index] = vampireDice();
     }
-    for (let index = 0; index < pool; index++) {
-        black[index] = Math.floor(Math.random() * 10) + 1;
-    }
+}
 
+function fillBlack(pool) {
+    for (let index = 0; index < pool; index++) {
+        black[index] = vampireDice();
+    }
+}
+
+function sortBoth() {
     red.sort(function(a, b){return b - a});
     black.sort(function(a, b){return b - a});
+}
 
+function countSuccesses() {
+    successes = 0;
+    crits = 0;
     for (let index = 0; index < red.length; index++) {
         if (red[index] >= 6) {
             successes = successes + 1;
@@ -44,10 +53,11 @@ formRoller.addEventListener("submit", (event) => {
         successes = successes + 2;
         crits = crits - 2;
     }
+}
 
+function printResults() {
     result.innerHTML = "<p>Successes: " + successes + "</p>";
     icons.innerHTML = "";
-
     for (let index = 0; index < red.length; index++) {
         switch (red[index]) {
             case 10:
@@ -71,7 +81,6 @@ formRoller.addEventListener("submit", (event) => {
         }
     }
     icons.innerHTML = icons.innerHTML + '<div style="width: 100px;"></div>';
-    let canReroll = 0;
     for (let index = 0; index < black.length; index++) {
         switch (black[index]) {
             case 10:
@@ -88,43 +97,34 @@ formRoller.addEventListener("submit", (event) => {
             case 3:
             case 2:
             case 1:
-                if (canReroll < 3) {
-                    canReroll += 1;
-                    icons.innerHTML = icons.innerHTML + '<img class="rerollable" src="/assets/normal-fail.png">';
-                } else {
-                    icons.innerHTML = icons.innerHTML + '<img src="/assets/normal-fail.png">';
-                }
+                icons.innerHTML = icons.innerHTML + '<img src="/assets/normal-fail.png">';
                 break;
         }
     }
+}
 
-    willpowerReRoll.addEventListener("click", () => {
-        if (canReroll > 0) {
-            let rerollable = document.querySelector(".rerollable");
-            let reroll = Math.floor(Math.random() * 10) + 1;
-            switch (reroll) {
-                case 10:
-                    successes = successes + 1;
-                    crits = crits + 1;
-                    if (crits >= 2) {
-                        successes = successes + 2;
-                        crits = crits - 2;
-                    }
-                    result.innerHTML = "<p>Successes: " + successes + "</p>";
-                    rerollable.src="/assets/normal-crit.png";
-                    break;
-                case 9:
-                case 8:
-                case 7:
-                case 6:
-                    successes = successes + 1;
-                    result.innerHTML = "<p>Successes: " + successes + "</p>";
-                    rerollable.src="/assets/normal-success.png";
-                    break;
-            }
-            rerollable.classList.remove("rerollable");
+formRoller.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const hunger = formRoller.querySelector("#hunger").value;
+    const pool = formRoller.querySelector("#pool").value - hunger;    
+
+    fillRed(hunger);
+    fillBlack(pool);
+    sortBoth();
+    countSuccesses();
+    printResults();
+    rerollable = 3;
+});
+
+willpowerReRoll.addEventListener("click", () => {
+    for (let index = 0; index < black.length; index++) {
+        if (black[index] <= 5 && rerollable > 0) {
+            black[index] = vampireDice();
+            rerollable--;
         }
-        canReroll -= 1;
-        setTimeout(250);
-    });
+    }
+    sortBoth();
+    countSuccesses();
+    printResults();
 });
