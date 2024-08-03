f42e3ec02180516047f297f15544b3f1922563c2
Balanceact <makildur@hotmail.com>
2024-08-03 19:53:50 +0200

    Added damage roller

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const formRoller = document.querySelector(".form-roller");
 const result = document.querySelector(".result");
 const icons =  document.querySelector(".icons");
 const willpowerReRoll = document.querySelector(".btn-WPRR");
+const formDamage = document.querySelector(".form-damage");
+const resultDamage = document.querySelector(".damage");
+const resultCrit = document.querySelector(".crit");
 
 let black = [];
 let red = [];
+let damage = [];
 let successes = 0;
 let crits = 0;
 let rerollable = 0;
+let damageCrits = 0;
+let damageSum = 0;
 
 function vampireDice() {
     return Math.floor(Math.random() * 10) + 1;
@@ -26,7 +32,7 @@ function clearResults() {
     successes = 0;
     crits = 0;
     rerollable = 0;
-    result.innerHTML = "<p>Successes: " + successes + "</p>";
+    result.innerHTML = "Successes: " + successes;
     icons.innerHTML = " ";
 }
 
@@ -71,7 +77,7 @@ function countSuccesses() {
 }
 
 function printResults() {
-    result.innerHTML = "<p>Successes: " + successes + "</p>";
+    result.innerHTML = "Successes: " + successes;
     icons.innerHTML = "";
     for (let index = 0; index < red.length; index++) {
         switch (red[index]) {
@@ -134,6 +140,54 @@ function rerollableNumber() {
     }
 }
 
+function damageDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function fillDamage(d6s) {
+    for (let index = 0; index < d6s; index++) {
+        damage[index] = damageDice();
+    }
+}
+
+function countDamage() {
+    damageCrits = 0;
+    damageSum = 0;
+    resultDamage.innerHTML = "Damage dealt: ";
+    resultCrit.innerHTML = " ";
+    for (let index = 0; index < damage.length; index++) {
+        switch (damage[index]) {
+            case 6:
+                damageCrits = damageCrits + 1;
+            case 5:
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+                damageSum = damageSum + damage[index];
+                resultDamage.innerHTML = resultDamage.innerHTML + damage[index];
+                if (index < damage.length - 1) {
+                    resultDamage.innerHTML = resultDamage.innerHTML + ", ";
+                }
+                break;
+        }
+    }
+    if (damageCrits >= 2) {
+        damageSum = damageSum + 5;
+        resultCrit.innerHTML = "CRIT!";
+    } else {
+        resultCrit.innerHTML = " ";
+    }
+
+    resultDamage.innerHTML = resultDamage.innerHTML + " = " + damageSum;
+}
+
+function clearDamage() {
+    for (let index = 0; index < damage.length; index++) {
+        damage[index] = 0;
+    }
+}
+
 formRoller.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -162,4 +216,14 @@ willpowerReRoll.addEventListener("click", () => {
     clearResults();
     countSuccesses();
     printResults();
+});
+
+formDamage.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const d6s = formDamage.querySelector(".select").value;
+
+    fillDamage(d6s);
+    countDamage();
+    clearDamage();
 });
